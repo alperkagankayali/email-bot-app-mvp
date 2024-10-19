9bbcf8e4bf376eb083b0071408592706853bacae
ondersahin <ondershin@gmail.com>
2024-10-20 00:54:45 +0300

    new template

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-   
-    return config;
-  },
-  experimental: {
-    esmExternals: "loose", // <-- add this
-    serverComponentsExternalPackages: ["mongoose"],
-  },
+ 
 };
 
 export default withNextIntl(nextConfig);
